bac9ead59b028a245176561052f98d675a67dc7b
George Kraft <george.kraft@canonical.com>
2022-10-28 13:14:33 -0500

    tox -e format

--- tests/unit/test_manipulations.py
+++ tests/unit/test_manipulations.py
@@ -88,7 +88,11 @@ def test_config_registry_pod_with_init_container():
     manifest.config = {"image-registry": rocks}
     c1 = dict(name="cool-pod", image="mcr.microsoft.com/awesome/image:1.0")
     c2 = dict(name="other-pod", image="gcr.io/other/image:2.0")
-    obj = from_dict(dict(apiVersion="v1", kind="Pod", spec=dict(containers=[c1], initContainers=[c2])))
+    obj = from_dict(
+        dict(
+            apiVersion="v1", kind="Pod", spec=dict(containers=[c1], initContainers=[c2])
+        )
+    )
 
     adjustment = ConfigRegistry(manifest)
     adjustment(obj)
