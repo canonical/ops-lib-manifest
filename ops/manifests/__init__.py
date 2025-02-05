e9ad4f32f6565929169f24a4cbee597e9865a1b0
Adam Dyess <adam.dyess@canonical.com>
2025-02-05 10:02:53 -0600

    Update API exposure

--- ops/manifests/__init__.py
+++ ops/manifests/__init__.py
@@ -1,4 +1,4 @@
-from ops.manifests.collector import Collector
+from ops.manifests.collector import Collector, ResourceAnalysis
 from ops.manifests.exceptions import ManifestClientError
 from ops.manifests.manifest import HashableResource, Manifests
 from ops.manifests.manipulations import (
@@ -17,11 +17,11 @@ __all__ = [
     "ConfigRegistry",
     "CreateNamespace",
     "HashableResource",
+    "ManifestClientError",
     "ManifestLabel",
     "Manifests",
     "Patch",
-    "update_tolerations",
+    "ResourceAnalysis",
     "SubtractEq",
-    "ManifestClientError",
     "update_tolerations",
 ]
